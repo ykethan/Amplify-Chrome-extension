0f0c8866ce8d5a12221b659bbe42c5706141ce52
ykethan <ykethan@amazon.com>
2022-07-22 22:13:45 -0500

    Revert to 606bfae0b05ae34a6017bf2461a480c4b3fd7e6d

---
+++ aws-exports-es5.js
@@ -0,0 +1,7 @@
+const awsmobile = {
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_ZCExq4zRH",
+    "aws_user_pools_web_client_id": "51nmi3oosuipq07tviqa4s4jek",
+  };
+  
+  exports.default = awsmobile;
---
+++ cypress/support/commands.js
@@ -0,0 +1,79 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+import Amplify, { Auth } from 'aws-amplify'
+
+Amplify.configure(Cypress.env('awsConfig'))
+
+// Amazon Cognito
+Cypress.Commands.add('loginByCognitoApi', (username, password) => {
+  const log = Cypress.log({
+    displayName: 'COGNITO LOGIN',
+    message: [`🔐 Authenticating | ${username}`],
+    // @ts-ignore
+    autoEnd: false,
+  })
+
+  log.snapshot('before')
+
+  const signIn = Auth.signIn({ username, password })
+
+  cy.wrap(signIn, { log: false }).then((cognitoResponse) => {
+    const keyPrefixWithUsername = `${cognitoResponse.keyPrefix}.${cognitoResponse.username}`
+
+    window.localStorage.setItem(
+      `${keyPrefixWithUsername}.idToken`,
+      cognitoResponse.signInUserSession.idToken.jwtToken
+    )
+
+    window.localStorage.setItem(
+      `${keyPrefixWithUsername}.accessToken`,
+      cognitoResponse.signInUserSession.accessToken.jwtToken
+    )
+
+    window.localStorage.setItem(
+      `${keyPrefixWithUsername}.refreshToken`,
+      cognitoResponse.signInUserSession.refreshToken.token
+    )
+
+    window.localStorage.setItem(
+      `${keyPrefixWithUsername}.clockDrift`,
+      cognitoResponse.signInUserSession.clockDrift
+    )
+
+    window.localStorage.setItem(
+      `${cognitoResponse.keyPrefix}.LastAuthUser`,
+      cognitoResponse.username
+    )
+
+    window.localStorage.setItem('amplify-authenticator-authState', 'signedIn')
+    log.snapshot('after')
+    log.end()
+  })
+
+ 
+})
