a074913fa65ef3942435b19ea941e7d7f35fb0a1
ykethan <ykethan@amazon.com>
2022-07-22 22:32:26 -0500

    Clean up ignored files

--- aws-exports-es5.js
+++
@@ -1,7 +0,0 @@
-const awsmobile = {
-    "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_ZCExq4zRH",
-    "aws_user_pools_web_client_id": "51nmi3oosuipq07tviqa4s4jek",
-  };
-  
-  exports.default = awsmobile;
