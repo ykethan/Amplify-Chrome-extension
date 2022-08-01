f5dd9ec4e7055d2050a0b5c7cc5dd7e4b04b558c
Kethan Sai Yaram <ykethan@amazon.com>
2022-08-01 12:01:17 -0500

    fixed auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "function": {
-        "testext38f2f2ab38f2f2abPreSignup": {
-            "Name": "string",
-            "Arn": "string",
-            "LambdaExecutionRole": "string",
-            "Region": "string"
-        },
-        "testext702fbf01PreSignup": {
-            "Name": "string",
-            "Arn": "string",
-            "LambdaExecutionRole": "string",
-            "Region": "string"
-        }
-    },
     "auth": {
         "testext702fbf01": {
             "IdentityPoolId": "string",
